aee866154412f6038cfdea7f52cfe3854f2b4b23
richard.wallnerstrom <riwajr@gmail.com>
2023-10-31 00:08:44 +0100

    hud visuals

--- script/HudController.js
+++ script/HudController.js
@@ -5,30 +5,33 @@ export default class HudController {
         this.mars = mars;
         this.smallHud = document.getElementById('smallHud');
         this.bigHud = document.getElementById('bigHud');
-
-
-    }
-    UpdateHud(fetchedUrl) {
-        fetch(fetchedUrl).then(page => page.text())
-            .then(text => {
-              this.smallHud.innerHTML = text;
-              this.smallHud.style.display = 'block'; 
-            })
     }
     DisplayHud() {
-        const distanceToEarth = this.spaceShip.model.position.distanceTo(
-            this.earth.model.position);
-        const distanceToMars = this.spaceShip.model.position.distanceTo(
-            this.mars.model.position); 
-        const fetchedUrl = (distanceToEarth < distanceToMars) 
-            ? '../pages/earth.html' : '../pages/mars.html'; 
-        if (distanceToEarth < 1200 || distanceToMars < 700) {
+        this.CalculateDistances(); 
+        const fetchedUrl = (this.distanceToEarth < this.distanceToMars) 
+            ? '../pages/earth.html' 
+            : '../pages/mars.html'; 
+        if (this.distanceToEarth < 1200 || this.distanceToMars < 700) {
             this.bigHud.style.display = 'none';
-            this.UpdateHud(fetchedUrl);
+            this.UpdateHud(fetchedUrl, this.smallHud);
         } 
         else {
             this.bigHud.style.display = 'block';
             this.smallHud.style.display = 'none';
         }
       }
+    UpdateHud(fetchedUrl, hud) {
+        fetch(fetchedUrl).then(page => page.text())
+            .then(text => {
+              hud.innerHTML = text;
+              hud.style.display = 'grid'; 
+            })
+    }
+    CalculateDistances() {
+        this.distanceToEarth = this.spaceShip.model.position.distanceTo(
+            this.earth.model.position);
+        this.distanceToMars = this.spaceShip.model.position.distanceTo(
+            this.mars.model.position); 
+    }
+    
 }

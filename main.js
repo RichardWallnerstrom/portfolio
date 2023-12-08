4f8c5647d95a82a4e726b65b3040b336e231f50f
richard.wallnerstrom <riwajr@gmail.com>
2023-12-08 17:38:01 +0100

    minor

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function updateContent(url, container) {
+function updateContent(url, container) { // TODO: Change duplicate names on containers. 
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -16,48 +16,42 @@ function updateContent(url, container) {
         })
 }
 function updateEventListeners(container) {
-        const spaceElement = document.getElementById("space-text")
-        const retroElement = document.getElementById("retro-text")
-        const musicElement = document.getElementById("music-text")
+    const spaceElement = document.getElementById("space-text");
+    const retroElement = document.getElementById("retro-text");
+    const musicElement = document.getElementById("music-text");
 
-        const workElement = document.getElementById("work-text")
-        const educationElement = document.getElementById("education-text")
-        const skillsElement = document.getElementById("skills-text")
-        const aboutElement = document.getElementById("about-text")
-
-        spaceElement.addEventListener('click', function() {
-            updateContent('pages/space.html', 'contentContainer');
-        });
-
-        retroElement.addEventListener('click', function() {
-            updateContent('pages/retro.html', 'contentContainer');
-        });
-
-        musicElement.addEventListener('click', function() {
-            updateContent('pages/music.html', 'contentContainer');
-        });
-
-
-        workElement.addEventListener('click', function() {
-            updateContent('pages/workExperience.html', 'contentContainer');
-        });
-
-        educationElement.addEventListener('click', function() {
-            updateContent('pages/education.html', 'contentContainer');
-        });
-
-        skillsElement.addEventListener('click', function() {
-            updateContent('pages/skills.html', 'contentContainer');
-        });
-        aboutElement.addEventListener('click', function() {
-            updateContent('pages/about.html', 'contentContainer');
-        });
-
-    
-}
-document.addEventListener("DOMContentLoaded", function() {
-    // Menu div on the left
-    const parentElement = document.getElementById("menuContainer")
+    const workElement = document.getElementById("work-text");
+    const educationElement = document.getElementById("education-text");
+    const skillsElement = document.getElementById("skills-text");
+    const aboutElement = document.getElementById("about-text");
+ 
+    spaceElement.addEventListener('click', function() {
+        loadContent('pages/space.html', 'contentContainer');
+    });
+ 
+    retroElement.addEventListener('click', function() {
+        loadContent('pages/retro.html', 'contentContainer');
+    });
+ 
+    musicElement.addEventListener('click', function() {
+        loadContent('pages/music.html', 'contentContainer');
+    });
+    workElement.addEventListener('click', function() {
+        loadContent('pages/workExperience.html', 'contentContainer');
+    });
+ 
+    educationElement.addEventListener('click', function() {
+        loadContent('pages/education.html', 'contentContainer');
+    });
+ 
+    skillsElement.addEventListener('click', function() {
+        loadContent('pages/skills.html', 'contentContainer');
+    });
+    aboutElement.addEventListener('click', function() {
+        loadContent('pages/about.html', 'contentContainer');
+    });
+ 
+    window.addEventListener('DOMContentLoaded', function() {
     const projectsElement = document.getElementById("projects-text")
     const aboutElement = document.getElementById("about-text")
 
@@ -66,10 +60,36 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     aboutElement.addEventListener('click', function() {
         updateContent('pages/menuAbout.html', 'menuContainer')
-    })
-    // Content div on the right
+    })    });
+}
+ 
+async function loadContent(page, container) {
+    try {
+        const response = await fetch(page);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const html = await response.text();
+        document.getElementById(container).innerHTML = html;
+    } catch (error) {
+        console.error('Error loading content:', error);
+    }
+}
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Menu div on the left
+//     // const parentElement = document.getElementById("menuContainer")
+//     const projectsElement = document.getElementById("projects-text")
+//     const aboutElement = document.getElementById("about-text")
+
+//     projectsElement.addEventListener('click', function() {
+//         updateContent('pages/menuProjects.html', 'menuContainer')
+//     })
+//     aboutElement.addEventListener('click', function() {
+//         updateContent('pages/menuAbout.html', 'menuContainer')
+//     })
+//     // Content div on the right
 
 
 
 
-})
+// })
